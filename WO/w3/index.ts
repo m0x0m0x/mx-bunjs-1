18cde503cd974805c3dcc113b60b3949dd291998
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-01-10 14:26:22 +0000

    🫦

--- WO/w3/index.ts
+++ WO/w3/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+// Web server
+
+const server = Bun.serve({
+  port: 8080,
+  fetch(req) {
+    return new Response("Lick her ass");
+  },
+});
+
+console.log("listening on localpanty")
