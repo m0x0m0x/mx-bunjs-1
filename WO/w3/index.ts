47d7c71458ea19d5f5db9aaade318018aa6ad1f1
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-01-10 14:29:43 +0000

    🫦

--- WO/w3/index.ts
+++ WO/w3/index.ts
@@ -3,8 +3,8 @@
 const server = Bun.serve({
   port: 8080,
   fetch(req) {
-    return new Response("Lick her ass");
+    return new Response("Lick her ass and pussy");
   },
 });
 
-console.log("listening on localpanty")
+console.log("listening on localpanty http://localhost:${server.port}")
