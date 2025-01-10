5d2fc35d877dbf5d42ed0b8a57e73495a48541f6
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-01-10 16:51:00 +0000

    🫦

--- WO/ely1/api1/src/index.ts
+++ WO/ely1/api1/src/index.ts
@@ -1,6 +1,9 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello fucker").listen(3000);
+const app = new Elysia().get("/", () => "Hello fucker")
+  .get('/post/:id', ({ params: { id } }) => { return { id: id, title: 'learnPanty', comment: 'Sniffing and licking' } })
+  .post('/post', ({ body: { id, title, comment } }) => { return { id: id, title: title, comment: comment } })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
