9cac9ada5d4a1e7492db64197f186fbbd5e0cdf0
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-01-10 16:57:44 +0000

    🫦

--- WO/ely1/api1/src/index.ts
+++ WO/ely1/api1/src/index.ts
@@ -3,6 +3,7 @@ import { Elysia } from "elysia";
 const app = new Elysia().get("/", () => "Hello fucker")
   .get('/post/:id', ({ params: { id } }) => { return { id: id, title: 'learnPanty', comment: 'Sniffing and licking' } })
   .post('/post', (body) => { return body })
+  .get('/track/*', () => { return 'Track Routes' })
   .listen(3000);
 
 console.log(
