27cf04452e2731258ee652ed8d4b16100ed39a83
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-01-10 16:10:40 +0000

    🫦

--- WO/w4/index.ts
+++ WO/w4/index.ts
@@ -1,1 +1,85 @@
-console.log("Hello via Bun!");
+// Web server
+
+import figlet from 'figlet'
+
+const server = Bun.serve({
+    port: 8080,
+    fetch(req) {
+        const url = new URL(req.url);
+
+        // Adding a route
+        if (url.pathname === "/") {
+            const body = figlet.textSync("SmellPanty")
+            return new Response(body);
+        }
+
+        // about page route
+        if (url.pathname === "/about") {
+            const body = "<h1>Lickasses</h1>"
+            return new Response(body, {
+                headers: {
+                    'Content-Type': 'text/html',
+                },
+            });
+        }
+
+        // Contact form render 
+        if (url.pathname === "/contact") {
+            return new Response(contactUsPage, {
+                headers: {
+                    'Content-Type': 'text/html',
+                },
+            });
+        }
+
+        //handle error 
+        if (url.pathname === "/error") {
+            throw new Error("Fucked");
+        }
+
+        // Streaming a file
+        if (url.pathname === "/lick") {
+            return new Response(Bun.file("lick.txt"));
+        }
+
+        return new Response("Not Found", { status: 404 });
+    },
+
+    error(error) {
+        return new Response(`<pre> ${error} \n ${error.stack} </pre>`, {
+            headers: {
+                'Content-Type': 'text/html',
+            },
+        })
+    },
+
+});
+
+console.log("listening on localpanty http://localhost:${server.port}")
+
+// This is the page 
+
+const contactUsPage = `<!DOCTYPE html>
+<html>
+  <head>
+    <title>Contact Us</title>
+    <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+>
+  </head>
+  <body>
+  <div class="container">
+    <h1>Contact Us</h1>
+    <form method="post" action="/contact">
+      <label for="name">Name:</label>
+      <input type="text" id="name" name="name" required>
+      <br>
+      <label for="email">Email:</label>
+      <input type="email" id="email" name="email" required>
+      <br>
+      <input type="submit" value="Submit">
+    </form>
+    </div>
+  </body>
+</html>`
