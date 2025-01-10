27a5826d9022b5d64bc581a9d8bf0288351a0323
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-01-10 14:23:00 +0000

    🫦

--- WO/w1/index.ts
+++ WO/w1/index.ts
@@ -1,2 +1,9 @@
 // Testing typescript in bun
 console.log("Hello via Bun!");
+
+const z = 10;
+const read = (text: string) => {
+  return text;
+};
+
+read("SmellPanty");
--- WO/w2/index.ts
+++ WO/w2/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+/***
+ * Testing bun second  version
+ */
+
+const z = 10;
+const read = (text: string) => {
+  return text;
+};
+
+const msg = read("SmellPanty");
+console.log(msg);
